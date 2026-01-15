33daef7b7cbd68882ca24b6cc9ec7b27134f0a05
jun837111 <58465326+jun837111@users.noreply.github.com>
2026-01-16 04:26:44 +0800

    Update maps.ts

--- src/data/maps.ts
+++ src/data/maps.ts
@@ -14,9 +14,28 @@ export interface MapData {
 }
 
 export const maps: MapData[] = [
-    { episode: 1, level: 40, name: '天使波利', maxStages: 1, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'boli' },
-    { episode: 15, level: 118, name: '尤那耶爾紀念區', maxStages: 4, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'Jonael Commemorative Orb' },
-    { episode: 15, level: 120, name: '坦尼爾1世紀念區', maxStages: 4, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'Taniel I Commemorative Orb' },
-    { episode: 15, level: 123, name: '緹玫勒斯寺院', maxStages: 4, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'Timerys Temple' }
-
+    //40lv
+    { episode: 1, level: 40, name: '天使波利', maxStages: 1, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'xxxx' },
+    { episode: 1, level: 41, name: '惡魔波利', maxStages: 1, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'xxxx' },
+    //60lv
+    { episode: 2, level: 60, name: '黃金蟲', maxStages: 1, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'xxxx' },
+    { episode: 2, level: 61, name: '蟻后', maxStages: 1, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'xxxx' },
+    { episode: 2, level: 62, name: '哥布靈首領', maxStages: 1, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'xxxx' },
+    { episode: 2, level: 63, name: '海盜之王', maxStages: 1, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'xxxx' },
+    { episode: 2, level: 64, name: '蜂后', maxStages: 1, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'xxxx' }, 
+    //80lv
+    { episode: 3, level: 80, name: '皮里恩', maxStages: 1, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'xxxx' }, 
+    { episode: 3, level: 81, name: '獸人酋長', maxStages: 1, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'xxxx' },
+    { episode: 3, level: 82, name: '俄塞里斯', maxStages: 1, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'xxxx' },
+    { episode: 3, level: 83, name: '獸人英雄', maxStages: 1, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'xxxx' },
+    //100lv
+    { episode: 4, level: 100, name: '虎王', maxStages: 1, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'xxxx' },
+    { episode: 4, level: 101, name: '月夜貓', maxStages: 1, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'xxxx' },
+    { episode: 4, level: 102, name: '死靈', maxStages: 1, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'xxxx' },
+    { episode: 4, level: 103, name: '古埃及王', maxStages: 1, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'xxxx' },
+    //120
+    { episode: 5, level: 120, name: '阿特羅斯', maxStages: 1, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'xxxx' },
+    { episode: 5, level: 121, name: '巴風特', maxStages: 1, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'xxxx' },
+    { episode: 5, level: 122, name: '德古拉男爵', maxStages: 1, isStarred: false, respawnTime: 25 * 60 + 15, imagePath: `${BASE_URL}maps/XXXXXX.webp`, enName: 'xxxx' }
+    
 ];
